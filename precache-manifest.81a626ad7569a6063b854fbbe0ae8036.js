10e6b1a2825000ec9e7c5ba7db318ca72819e73e
Eduardo Moreira <contato@eduardoem.com.br>
2020-02-21 09:10:06 -0300

    Updates

---
+++ precache-manifest.81a626ad7569a6063b854fbbe0ae8036.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f0d0b9666e3593c9af601dafe6a0bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d00dadd77d45d683aa0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a3228f66e4caff00139c",
+    "url": "/static/js/2.6b03f0e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6b03f0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d00dadd77d45d683aa0",
+    "url": "/static/js/main.be981920.chunk.js"
+  },
+  {
+    "revision": "1f47fef2c88e9131c3ca",
+    "url": "/static/js/runtime-main.85626191.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
